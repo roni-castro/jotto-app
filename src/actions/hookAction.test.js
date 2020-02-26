7341915b27339df66be12b38363f411bf55ced02
Roni <roni.c.castro@gmail.com>
2020-02-25 23:27:45 -0300

    Add getSecretWord hookAction

---
+++ src/actions/hookAction.test.js
@@ -0,0 +1,28 @@
+import moxios from 'moxios'
+import getSecretWord from './hookAction'
+
+describe('hookAction test', () => {
+  beforeEach(() => {
+    moxios.install()
+  })
+
+  afterEach(() => {
+    moxios.uninstall()
+  })
+
+  test('calls the setSecretWord  callback on axios response', async () => {
+    const secretWord = 'party'
+
+    moxios.wait(() => {
+      const request = moxios.requests.mostRecent()
+      request.respondWith({
+        status: 200,
+        response: secretWord,
+      })
+    })
+
+    const mockSetSecretWord = jest.fn()
+    await getSecretWord(mockSetSecretWord)
+    expect(mockSetSecretWord).toHaveBeenCalledWith(secretWord)
+  })
+})
