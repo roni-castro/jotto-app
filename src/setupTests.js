edc58c61ebbcbc455fa629a4786cac4897ce36de
Roni <roni.c.castro@gmail.com>
2020-03-29 23:32:20 -0300

    Install jest-styled-components dev dependency

--- src/setupTests.js
+++ src/setupTests.js
@@ -5,5 +5,6 @@
 import '@testing-library/jest-dom/extend-expect';
 import Enzyme from 'enzyme';
 import EnzymeAdapter from 'enzyme-adapter-react-16';
+import 'jest-styled-components'
 
 Enzyme.configure({ adapter: new EnzymeAdapter() });
